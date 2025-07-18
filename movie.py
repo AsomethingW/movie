749deb0dda4c08faf50341ec62c627e30577933b
AsomethingW <albert.wakuteka@hgs.hiddengeniusproject.org>
2025-07-18 10:40:12 -0400

    Update movie.py

--- movie.py
+++ movie.py
@@ -3,38 +3,49 @@ class Movie:
         self.title = title
         self.director = director
         self.duration = duration
-        self.watched = watched
-        self.rating = rating
+        self.watched = False
+        self.rating = None
     
     def __str__(self):
+        status = "Watched" if self.watched else "Not watched yet"
+        rating = self.rating if self.rating is not None else "N/A"
         return f"Movie: {self.title} directed by {self.director}. Duration: {self.duration}. Status: {self.watched}. Rating: {self.rating}. "
     
-    def mark_as_watched(rating):
-        watched = True
-
-        rating = 9.5
-    mark_as_watched()
-
-    def is_long_movie():
-        
-        duration = 123
-
-        if duration > 150:
-            return f"True"
+    def mark_as_watched(self, rating):
+        if 0<= rating <=10:
+            self.watched = True
+            self.rating = rating
         else:
-            return f"False"
-    is_long_movie()
+            print("Invalid rating. Must be between 0 and 10.")
 
-    def update_rating(new_rating):
-        watched = True
+    def is_long_movie(self):
+        
+        return self.duration > 150
 
-        if watched == True:
-            rating = input("How do you rate the movie?")
-    update_rating()
 
-    def reset_watch_status():
-        watched = False
+    def update_rating(self, new_rating):
 
-        rating = None
-    reset_watch_status()
+        if self.watched:
+            self.rating = new_rating
+        else:
+            print("Cannot update rating. Movie has not been watched yet.")
+
+    def reset_watch_status(self):
+        self.watched = False
+
+        self.rating = None
+
+
 movie1 = Movie(f"Going Away, Gary Gibson, {4},")
+
+print(movie1)
+
+movie1.mark_as_watched(8)
+
+movie1.update_rating(10)
+
+print(movie1.is_long_movie())
+
+movie1.reset_watch_status()
+
+print(movie1)
